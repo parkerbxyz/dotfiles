b058c2cbf95b907d5cd9d769b4d0d2d309777870
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2022-09-14 21:34:36 -0700

    Initialize rbenv and pyenv in interactive sessions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+    source (rbenv init - | psub)
+    source (pyenv init - | psub)
 end
