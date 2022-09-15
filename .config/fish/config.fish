29ce27ded5a6e19373f5e046c62f1a1a84ff1bb3
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2022-09-14 22:04:51 -0700

    Enable experimental VS Code fish shell integration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,9 @@ if status is-interactive
     command -q pyenv && pyenv init - | source
         # Fix brew doctor's pyenv "config" scripts warning
         and alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
+
+    # VS Code shell integration (experimental)
+    # https://code.visualstudio.com/docs/terminal/shell-integration
+    string match -q "$TERM_PROGRAM" "vscode"
+        and . (code --locate-shell-integration-path fish)
 end
