a3b782156ca60f6438e8f9f0e7f9ba74414b448e
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2022-09-14 22:41:13 -0700

    Enable 1Password CLI shell completion

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,4 +11,7 @@ if status is-interactive
     # https://code.visualstudio.com/docs/terminal/shell-integration
     string match -q "$TERM_PROGRAM" "vscode"
         and . (code --locate-shell-integration-path fish)
+
+    # 1Password CLI shell completion (https://developer.1password.com/docs/cli)
+    command -q op && op completion fish | source
 end
