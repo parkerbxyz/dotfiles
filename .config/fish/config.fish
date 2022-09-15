ea953382036d08c8090957fb42fa9714eb57375e
Parker Brown <17183625+parkerbxyz@users.noreply.github.com>
2022-09-14 22:04:11 -0700

    Fix brew doctor's pyenv "config" scripts warning

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,4 +3,6 @@ if status is-interactive
     command -q starship && starship init fish | source
     command -q rbenv && rbenv init - | source
     command -q pyenv && pyenv init - | source
+        # Fix brew doctor's pyenv "config" scripts warning
+        and alias brew="env PATH=(string replace (pyenv root)/shims '' \"\$PATH\") brew"
 end
